5855cd2db2f894e210596013e39ad0ce9571c134
ajreyna <ajreyna88@gmail.com>
2019-06-12 15:45:49 -0500

    updated points

--- masterMind/masterMind/Program.cs
+++ masterMind/masterMind/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
   using System;
 
 
@@ -6,7 +6,7 @@ namespace masterMind
 {
     class program
     {
-        
+
         static int[] hint;
         static int tryCount;
         string[][] board;
@@ -14,7 +14,7 @@ namespace masterMind
         static string[] secret = new string[codeLength];
         static string[] colorArray = new string[] { "Red", "Yellow", "Blue" };
         static bool gameOver = false;
-
+        //string IsCorrectAnswer;
 
         static void Main(string[] args)
         {
@@ -25,16 +25,162 @@ namespace masterMind
                 secret[i] = colorArray[rnd.Next(0, 2)];
 
             }
-            while (gameOver == false)
+            while (gameOver == false && allowedAttempsts <= 4)
             {
                 // ask user for input
                 Console.WriteLine("Enter your guess:");
                 string[] guess = Console.ReadLine().Split(' ');
-                gameOver = isCorrectAnswer(guess);
+               // gameOver = IsCorrectAnswer(guess);
             }
         }
-        public static bool IsCorrectAnswer(string[] userGuess)
+        //public static bool IsCorrectAnswer(string[] userGuess)
+        //{
+          //  if ()
+        //}
+    }
+}
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
+            string[] colors = { "red", "blue", "green" };
+
+            int length = colors.GetLength(0);
+            Random rnd = new Random();
+            // creates a number 0,1 or 2
+            int x = rnd.Next(0, 3);
+            int y = rnd.Next(0, 3);
+            //int x = 0, y = 0;
+            string userX, userY;
+            bool game = true;
+            int condition = 0;
+            int turns = 0;
+
+            //Game Instructions
+            Console.Write("Mastermind!\n \n");
+            Console.WriteLine("The computer will choose two colors Red, blue, or green.\n");
+            Console.WriteLine("You must guess the correct colors and the correct placement first, and second.");
+            Console.WriteLine("If neither color nor placement are correct the computer will say 0-0\n");
+            Console.WriteLine( "If one color is correct but in the wrong place the computer will say 1-0");
+            Console.WriteLine("if one color is correct and in the correct position the computer will say 1-1");
+            Console.WriteLine("if both colors are correct but in the wrong positions the computer will say 2-0");
+            Console.WriteLine("If you guess both correctly and in the correct positions the computer will say 2-2 and you win!");
+
+
+            //game logic
+            while (game==true)
+            {
+                turns++;
+
+                Console.Write("Please enter your first color: ");
+                userX = Console.ReadLine();
+                userX = userX.ToLower();
+                if (userX != "red" && userX != "blue" && userX != "green")
+                {
+                    Console.Write("Invalid input. Please enter red, blue, or green");
+                    userX = Console.ReadLine();
+                    userX = userX.ToLower();
+                }
+                Console.Write("Please enter your second color: ");
+                userY = Console.ReadLine();
+                userY = userY.ToLower();
+                if (userY != "red" && userY != "blue" && userY != "green")
+                {
+                    Console.Write("Invalid input. Please enter red, blue, or green");
+                    userY = Console.ReadLine();
+                    userY = userY.ToLower();
+                }
+                condition = eval(colors[x], colors[y], userX, userY);
+                switch (condition)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("0-0");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("1-0");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("1-1");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("0-2");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("2-2");
+                            Console.WriteLine("You won in " + turns + " turns!");
+                            game = false;
+                            break;
+                        }
+
+                }
+
+            }
+            Console.ReadLine();
 
         }
+
+
+        static int eval(string x, string y, string userX, string userY)
+        {
+            if ((userX != x && userX != y) && (userY != x && userY != y))
+            {
+                return 0;
+            }
+            else if (userX == y && userY == x)
+            {
+                if (userX == x && userY == y) { return 4; }
+                else return 3;
+            }
+            else if (userX == x && userY != y)
+            {
+                return 2;
+            }
+            else if (userX != x && userY == y)
+            {
+                return 2;
+            }
+            else if (userX == y)
+            {
+                return 1;
+            }
+            else if (userY == x)
+            {
+                return 1;
+            }
+            else if (userX == x && userY != y)
+            {
+                return 2;
+            }
+            else if (userX != x && userY == y)
+            {
+                return 2;
+            }
+            else if (userX == x && userY == y)
+            {
+                return 4;
+            }
+            return 0;
+        }
     }
+}
---
+++ points/points/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+
+namespace points
+{
+    class MainClass
+    {
+        // public static void Main(string[] args)
+
+        
+
+
+        public class Point2D
+        {
+            int x;
+            int y;
+
+            public Point2D()
+            {
+                // your code here
+                int X;
+                int Y;
+
+            }
+
+            public Point2D(int x, int y)
+            {
+                // your code here  
+                X = x;
+                Y = y;
+
+            }
+
+            public override bool Equals(object o)
+            {
+                if(o.GetType().Equals(this.GetType()))
+                {
+                    Point2D point = (Point2D)o;
+                    return this.x = point.X && this.Y == point.Y;
+                }
+                else
+                {
+                    return false;
+                }
+            
+            }
+
+            public override string ToString()
+            {
+                return $"{(X), (Y)"};
+            }
+
+
+
+        }
+
+        public class Point3D : Point2D
+        {
+            public Point3D()
+            {
+                //var Z = 0;
+                // your code here
+            }
+            public Point3D(int x, int y, int z) : base(x, y)
+            {
+                // your code here
+            }
+
+            // your code here
+        }
+
+        public class Program
+        {
+            public static void Main(String[] args)
+            {
+                Point2D p1 = new Point2D();
+                Point2D p2 = new Point2D(7, 5);
+                Point2D p3 = new Point2D(-2, 4);
+                Point2D p4 = new Point2D(7, 5);
+
+                Console.WriteLine("p1 = {0}", p1);
+                Console.WriteLine("p2 = {0}", p2);
+                Console.WriteLine("p3 = {0}", p3);
+                Console.WriteLine("p4 = {0}", p4);
+
+                Console.WriteLine("p2 == p3? {0}", p2 == p3);
+                Console.WriteLine("p2.Equals(p3)? {0}", Point2D.Equals(p2, p3));
+                Console.WriteLine("p2 == p4? {0}", p2 == p4);
+                Console.WriteLine("p2.Equals(p4)? {0}", Point2D.Equals(p2, p4));
+
+                Console.WriteLine("========");
+
+                Point3D p5 = new Point3D();
+                Point3D p6 = new Point3D(7, 5, 12);
+                Point3D p7 = new Point3D(-2, 4, -4);
+                Point3D p8 = new Point3D(7, 5, 12);
+
+                Console.WriteLine("p5 = {0}", p5);
+                Console.WriteLine("p6 = {0}", p6);
+                Console.WriteLine("p7 = {0}", p7);
+                Console.WriteLine("p8 = {0}", p8);
+
+                Console.WriteLine("p6 == p7? {0}", p6 == p7);
+                Console.WriteLine("p6.Equals(p7)? {0}", Point2D.Equals(p6, p7));
+                Console.WriteLine("p6 == p8? {0}", p6 == p8);
+                Console.WriteLine("p6.Equals(p8)? {0}", Point2D.Equals(p6, p8));
+
+                Console.WriteLine("========");
+            }
+        }
+
+    }
+}
