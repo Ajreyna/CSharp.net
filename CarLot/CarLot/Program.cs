38c789d13adb761df4097a1a9622ecc4499d01ac
ajreyna <ajreyna88@gmail.com>
2019-08-01 20:46:02 -0500

    refactored Carlot

--- CarLot/CarLot/Program.cs
+++ CarLot/CarLot/Program.cs
@@ -5,36 +5,50 @@ namespace CarLot
 {
     class MainClass
     {
+        //Main as used here implements the classes and methods for the rest of the program
         public static void Main(string[] args)
         {
 
             Console.WriteLine("What is the name of the car lot?");
             string response1 = Console.ReadLine();
+            //CarLot cars = new CarLot(response1);
+            Console.WriteLine("The name of this carlot is {0}. \n \n", response1);
+            Console.WriteLine("The inventory of this carlot includes");
             CarLot cars = new CarLot(response1);
+            
 
             cars.AddVehicle(new Car("Coupe", 2, "1234566", "ford", "mustang", 25000));
+            cars.AddVehicle(new Car(CarType: "Sedan", NumOfDoors: 4, licenseNumber:"AVB33KXR", Make:"Chrysler", Model:"New Yorker Brougham", Price: 10000));
+            cars.AddVehicle(new Truck(licenseNumber:"WTF190XRF", Make:"Chevrolet", Model:"Silverado", Price:90000, BedSize:"Long Bed"));
+
             cars.PrintVehicles();
         }
 
 
     }
 
+    //Below CarLot holds a list for vehicles and uses constructors to Add and Print the cars.
     class CarLot
     {
         string name;
+        
         List<Vehicle> vehicles = new List<Vehicle>();
 
+        //Sets name
         public CarLot(string name)
         {
             this.name = name;
         }
+        
+        //Adds vehicles to list
         public void AddVehicle(Vehicle NewVehicle)
         {
 
             vehicles.Add(NewVehicle);
            
         }
-
+        
+        // PrintVehicles uses a foreach loop to write the list to console.
         public void PrintVehicles()
         {
             foreach (Vehicle item in vehicles)
@@ -47,19 +61,25 @@ namespace CarLot
 
         //ans += vehicles;
     }
+    
+    //Here the class Vehicle gets and sets the make, model, plate number and price 
     public abstract class Vehicle
     {
         public string PlateNumber { get; set; }
         public string make { get; set; }
         public string model { get; set; }
         public int price { get; set; }
+        
+        
 
-        public Vehicle(string licenseNumber, string Make, string Model, int Price )
+        public Vehicle(string licenseNumber, string Make, string Model, int Price)
         {
             PlateNumber = licenseNumber;
             make = Make;
             model = Model;
             price = Price;
+            
+
 
         }
 
@@ -77,15 +97,17 @@ namespace CarLot
     }
     class Truck : Vehicle
     {
-        public int BedSize { get; set; }
-
-        public Truck(string licenseNumber, string Make, string Model, int Price, int BedSize) : base(licenseNumber, Make, Model, Price)
+        private string BedSize = "LongBed";
+        //public string BedSize { get; set; }
+        
+        //The Truck method sets licenseNumber, Make, Model, Price and BedSize.
+        public Truck(string licenseNumber, string Make, string Model, int Price, string BedSize) : base(licenseNumber, Make, Model, Price)
         {
             this.BedSize = BedSize;
-            //this.PlateNumber = PlateNumber;
-            //this.make = make;
-            //this.model = model;
-            //this.price = price;
+            this.PlateNumber = PlateNumber;
+            this.make = make;
+            this.model = model;
+            this.price = price;
         }
 
 
@@ -97,6 +119,7 @@ namespace CarLot
         string Cartype;
         int NumOfDoors;
 
+        //Here The type, number of doors, license number, make , model and price are set to "Car".
         public Car(string CarType, int NumOfDoors, string licenseNumber, string Make, string Model, int Price) : base(licenseNumber, Make, Model, Price)
         {
             this.Cartype = CarType;
