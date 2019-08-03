08d506824c5577325601e84b34e1b0ef9c7454a1
ajreyna <ajreyna88@gmail.com>
2019-08-03 14:19:41 -0500

    finished rebuild of inventory

--- Inventory2/Inventory2/Program.cs
+++ Inventory2/Inventory2/Program.cs
@@ -1,121 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+using CarLot2;
 
-namespace Inventory2
+namespace CarLot2
 {
-    class MainClass
+    public class Program
+
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            List<IRentable> description = new List<IRentable>();
-            description.Add(new Boat("Bass"));
-            description.Add(new Car("Convertible"));
-            description.Add(new House("2b 2ba"));
 
-           //Console.WriteLine("This rate for this boat is:{0} ", _hourlyRate);
+            List<IRentable> carlot = new List<IRentable>();
+            carlot.Add(new Boat("SpeedBoat3000"));
+            carlot.Add(new Boat(initName: "Bass Hunter Deluxe"));
+            carlot.Add(new Car(initName: "Coupe Sport Convertible"));
+            carlot.Add(new Car(initName: "4x4 Pickup Truck"));
+            carlot.Add(new Car("Lincoln Continental"));
 
 
-            foreach (IRentable item in description)
+            carlot.Add(new House("Ranch Rambler 9000"));
+            carlot.Add(new House(initName: "4 Bed 3 Bath Beach House"));
+            carlot.Add(new House(initName: "Downtown Loftspace "));
+
+            Console.WriteLine("       Welcome to the rental MegaCenter! \n " +
+                              "\n   Please press any key after each entry" +
+                              " to see the next... \n\n");
+            foreach (IRentable rentable in carlot)
             {
-               //Console.WriteLine(item.GetDailyRate());
-               //Console.WriteLine(item.GetDescription());
 
 
-                //Prof. Zhengs Examples
-                Console.WriteLine($"Rental type: {item.GetType()}");
-                Console.WriteLine(item.GetDescription());
-                Console.WriteLine(item.GetDailyRate());
-                Console.WriteLine("++++++++++");
+                rentable.GetDescription();
 
-
-            }
-        }
-
-        public interface IRentable
-        {
-            decimal GetDailyRate();
-            string GetDescription();
-        }
+                rentable.GetDailyRate();
 
 
 
-        public class Boat : IRentable
-        {
-            private int _hourlyRate = 20;
-            private string _description;
-
-       
-            public Boat(string description)
-            {
-                _description = description;
+                Console.ReadKey();
             }
 
 
-            public decimal GetDailyRate()
-            {
-                return _hourlyRate * 24;
-            }
-
-            public string GetDescription()
-            {
-                return _description;
-            }
 
 
-        }
-    
-
-    
-
-
-        public class House : IRentable
-        {
-           private decimal _weeklyRate = 1000;
-           private string _description;
-
-
-            public House(string description)
-            {
-                _description = description;
-            }
-
-            public decimal GetDailyRate()
-            {
-                //Console.WriteLine("The rate for this house is:{0} ", _weeklyRate);
-               decimal drate = _weeklyRate / 7 ;
-                return Decimal.Round(drate, 2);
-            }
-
-            public string GetDescription()
-            {
-                //Console.WriteLine("This house has {0}", )
-                return _description;
-            }
-        }
-    
-
-
-        public class Car : IRentable
-        {
-            private int _dailyRate = 80;
-            private string _description;
-
-            public Car(string description)
-            {
-                _description = description;
-            }
-
-            public decimal GetDailyRate()
-            {
-                //Console.WriteLine("The rate for this car is: {0}",)
-                return _dailyRate;
-            }
-
-            public string GetDescription()
-            {
-                //Console.WriteLine("This car is a: {0}", );
-                return _description;
-            }
 
         }
 
@@ -123,4 +49,145 @@ namespace Inventory2
 
 
     }
+
+    #region IRenatble interface
+    public interface IRentable
+    {
+        void GetDailyRate();
+
+        void GetDescription();
+
+
+
+
+    }
 }
+
+
+#endregion
+
+//
+//
+//
+
+
+
+#region Boat House and Car Classes
+// 
+//
+//
+
+public class Boat : IRentable
+{
+    //Boat class must internally store a hourly rate
+
+
+    double rate = 100;
+
+    String name;
+    public Boat(string initName)
+    {
+        this.name = initName;
+
+    }
+
+    public void GetDailyRate()
+    {
+
+        Random rand = new Random();
+        rate = rand.Next(0, 200);
+
+        Console.WriteLine("The daily rate is ${0}.", rate);
+
+        Console.WriteLine("\n ");
+
+
+    }
+
+
+    public void GetDescription()
+    {
+
+        Console.WriteLine("This boat is the {0}! \n", name);
+
+    }
+
+
+
+
+
+    //public void GetDescription()
+    //{
+
+    //}
+
+}
+
+
+public class Car : IRentable
+{
+    String name;
+    public Car(string initName)
+    {
+
+        this.name = initName;
+
+    }
+
+    public void Message()
+    {
+        Console.WriteLine("The cars for rent are as follows /n" +
+                          ": Please press any key after each entry to see" +
+                          " the next...");
+    }
+    public void GetDescription()
+    {
+        Console.WriteLine("The cars in our inventory are: {0} \n", name);
+
+
+
+
+    }
+
+    public int rate = 100;
+
+
+    public void GetDailyRate()
+    {
+        Random rand = new Random();
+        rate = rand.Next(0, 200);
+        Console.WriteLine("The daily rate for this fine automobile is ${0} \n \n", rate);
+    }
+}
+
+
+public class House : IRentable
+{
+    public int rate;
+    public string name;
+
+    public House(String initName)
+    {
+        this.name = initName;
+
+    }
+    public void GetDescription()
+    {
+        Console.WriteLine("This rental house is {0} \n", name);
+
+    }
+
+    public void GetDailyRate()
+    {
+        Random rand = new Random();
+        rate = rand.Next(100, 500);
+
+        Console.WriteLine("The daily rate is ${0} \n \n", rate);
+    }
+}
+
+
+
+
+#endregion
+
